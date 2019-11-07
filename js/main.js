cf9953d48297455cfd5c1bf2f86938c50756f13d
Jacob M <moe.jacobw@gmail.com>
2019-11-07 11:40:17 -0600

    change names from opt to more specific names

--- js/main.js
+++ js/main.js
@@ -6,66 +6,65 @@
      $('input[name="check-term"]').on('click', function () {
          if ($(this).val() == 'opt1') {
              $('#terms-boxes').show();
-             $('#opt1').show();
-             $('#opt2').hide();
-             $('#opt3').hide();
-             $('#opt4').hide();
+             $('#ingredientInput').show();
+             $('#cocktailNameInput').hide();
+             $('#beginingLetterInput').hide();
+             $('#idInput').hide();
              $('#random-submit').hide();
          } else if ($(this).val() == 'opt2') {
              $('#terms-boxes').show();
-             $('#opt2').show();
-             $('#opt1').hide();
-             $('#opt3').hide();
-             $('#opt4').hide();
+             $('#cocktailNameInput').show();
+             $('#ingredientInput').hide();
+             $('#beginingLetterInput').hide();
+             $('#idInput').hide();
              $('#random-submit').hide();
          } else if ($(this).val() == 'opt3') {
              $('#terms-boxes').show();
-             $('#opt3').show();
-             $('#opt1').hide();
-             $('#opt2').hide();
-             $('#opt4').hide();
+             $('#beginingLetterInput').show();
+             $('#ingredientInput').hide();
+             $('#cocktailNameInput').hide();
+             $('#idInput').hide();
              $('#random-submit').hide();
          } else if ($(this).val() == 'opt4') {
              $('#terms-boxes').show();
-             $('#opt4').show();
-             $('#opt1').hide();
-             $('#opt2').hide();
-             $('#opt3').hide();
+             $('#idInput').show();
+             $('#ingredientInput').hide();
+             $('#cocktailNameInput').hide();
+             $('#beginingLetterInput').hide();
              $('#random-submit').hide();
          } else if ($(this).val() == 'opt5') {
              $('#terms-boxes').hide();
-             $('#opt4').show();
-             $('#opt1').hide();
-             $('#opt2').hide();
-             $('#opt3').hide();
-             $('#opt4').hide();
+             $('#ingredientInput').hide();
+             $('#cocktailNameInput').hide();
+             $('#beginingLetterInput').hide();
+             $('#idInput').hide();
              $('#random-submit').show();
          }
      })
  }
 
  function formatDrinkSearch() {
-     let opt1 = document.getElementById('check-ingredient');
-     let opt2 = document.getElementById('check-cocktail-name');
-     let opt3 = document.getElementById('check-begining-letter');
-     let opt4 = document.getElementById('check-id');
+     let ingredientInput = document.getElementById('check-ingredient');
+     let cocktailNameInput = document.getElementById('check-cocktail-name');
+     let beginingLetterInput = document.getElementById('check-begining-letter');
+     let idInput = document.getElementById('check-id');
      let opt5 = document.getElementById('check-random');
      let search;
      let query;
      let param;
-     if (opt1.checked) {
+     if (ingredientInput.checked) {
          search = "filter.php?i=";
          query = $('#search-ingredient').val();
          param = search + query;
-     } else if (opt2.checked) {
+     } else if (cocktailNameInput.checked) {
          search = "search.php?s=";
          query = $('#search-cocktail-name').val();
          param = search + query;
-     } else if (opt3.checked) {
+     } else if (beginingLetterInput.checked) {
          search = "search.php?f=";
          query = $('#search-begining-letter').val();
          param = search + query;
-     } else if (opt4.checked) {
+     } else if (idInput.checked) {
          search = "lookup.php?i=";
          query = $('#search-id').val();
          param = search + query;
@@ -99,22 +98,22 @@
 
  function displayResults(responseJson) {
      $('#results-list').empty();
-     let opt1 = document.getElementById('check-ingredient');
-     let opt2 = document.getElementById('check-cocktail-name');
-     let opt3 = document.getElementById('check-begining-letter');
-     let opt4 = document.getElementById('check-id');
+     let ingredientInput = document.getElementById('check-ingredient');
+     let cocktailNameInput = document.getElementById('check-cocktail-name');
+     let beginingLetterInput = document.getElementById('check-begining-letter');
+     let idInput = document.getElementById('check-id');
      let opt5 = document.getElementById('check-random');
      //maxResult is 15 because that is how many ingredients and measures the Json comes back with
      const maxResult = 15;
      let drink = responseJson.drinks;
      
-     if (opt1.checked) {
+     if (ingredientInput.checked) {
          for (let i = 0; i < drink.length; i++) {
              $('#results-list').append(
                  `<li><h3>${drink[i].strDrink}</h3><p><span>Drink ID: </span>${drink[i].idDrink}</p><p><img src="${drink[i].strDrinkThumb}" alt="${drink[i].strDrink}"></p>`)
          }
          $('#results').removeClass('hidden');
-     } else if (opt2.checked || opt3.checked || opt4.checked || opt5.checked) {
+     } else if (cocktailNameInput.checked || beginingLetterInput.checked || idInput.checked || opt5.checked) {
          for (let i = 0; i < drink.length; i++) {
              $('#results-list').append(
                  `<li><div class="drink-list"><div class="info"><p><span>Drink Name: </span>${drink[i].strDrink}</p><p><span>Drink ID: </span>${drink[i].idDrink}</p><p><span>Category: </span>${drink[i].strCategory}</p><p><span>Non\/Alcoholic: </span>${drink[i].strAlcoholic}</p><p><span>Glass Type: </span>${drink[i].strGlass}</p></div><div class="drinkInstructions"><div class="image half-width"><p><img src="${drink[i].strDrinkThumb}" alt="The Cocktail is  ${drink[i].strDrink}"></p></div><div class="drink-mix half-width"><ul id="measurements-${i}"><h4>Measurements</h4></ul><ul id="ingredients-${i}"><h4>Ingredients</h4></ul></div><div class="instructions"><p><span>Instructions: </span>${drink[i].strInstructions}</p></div></div></div></li>`)
